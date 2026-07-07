11f139e8e88619e6048b052619cfa0b912417484
LY67198 <2854538716@qq.com>
2026-07-07 09:43:34 +0800

    feat: add Book and Document models, add is_admin to User

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,8 @@
 
 from .appointment import Appointment, AppointmentStatus
 from .base import Base, new_uuid, utcnow
+from .book import Book
+from .document import DocSourceType, Document
 from .floor import Floor
 from .seat import Seat, SeatStatus
 from .seat_time_slot import SeatTimeSlot, TimeSlot
@@ -13,6 +15,9 @@ __all__ = [
     "new_uuid",
     "utcnow",
     "User",
+    "Book",
+    "Document",
+    "DocSourceType",
     "Floor",
     "Zone",
     "ZoneType",
